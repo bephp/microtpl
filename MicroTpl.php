0a2fbc33c4e2982bf7238102d6108a09b8ffc375
U-lloydzhou\lloyd <lloydzhou@qq.com>
2013-12-14 06:16:36 +0800

    1. catch error when parse_xml, 2. replace tab, 3. using function empty to replace count to test array

--- MicroTpl.php
+++ MicroTpl.php
@@ -1,94 +1,91 @@
 <?php 
 class MicroTpl{
-	public $depth;
-	public $repeat = array();
-	public $condition = array();
-	public $content = array();
-	public $replace = array();
-	public $parser;
-	const PREFIX = 'tal:';
-	
-	public function __construct() {
-		$this->parser = xml_parser_create();
+    public $depth;
+    public $repeat = array();
+    public $condition = array();
+    public $content = array();
+    public $replace = array();
+    public $parser;
+    const PREFIX = 'tal:';
+    
+    public function __construct() {
+        $this->parser = xml_parser_create();
         xml_set_object($this->parser, $this);
-		xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
+        xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
         xml_set_element_handler($this->parser, "tag_open", "tag_close");
         xml_set_character_data_handler($this->parser, "cdata");
-	}
-	public function __destruct() {
-		xml_parser_free($this->parser);
-	}
-	public static function parse($source) {
-		$view = $source. 'c';
-		if (@filemtime($source)>@filemtime($view)) {
-			$parser = new self();
-			@file_put_contents($view, $parser->_parse(@file_get_contents($source)));
-		}
-		return $view;
-	}
-    protected function _parse($template) {
-		preg_match('@<\!DOCTYPE[^>]*>@', $template, $doctype);
-		ob_start();
-		echo (isset($doctype[0]) ? $doctype[0] . "\n" : '');
-        xml_parse($this->parser, $template);
-		echo "\n";
-		return ob_get_clean();
     }
-	public static function render($view, $data = array(), $layout = '') {
-		if(!is_file($view)) throw new CException('View file "'. $view. '" does not exist.');
-		extract($data);
-		ob_start();
-		include (self::parse($view));
-		$content = ob_get_clean();
-		if (!is_file($layout)) echo $content;
-		else include (self::parse($layout));
-	}
+    public function __destruct() {
+        xml_parser_free($this->parser);
+    }
+    public static function parse($source) {
+        $view = $source. 'c';
+        if (@filemtime($source)>@filemtime($view)) {
+            $parser = new self();
+            @file_put_contents($view, $parser->_parse(@file_get_contents($source)));
+        }
+        return $view;
+    }
+    protected function _parse($template) {
+		return (ob_start() &&  xml_parse($this->parser, $template))
+			? (preg_match('@<\!DOCTYPE[^>]*>@', $template, $doctype) ? $doctype[0] . "\n" : ''). ob_get_clean(). "\n"
+			: $template;
+    }
+    public static function render($view, $data = array(), $layout = '') {
+        if(!file_exists($view)) throw new CException('View file "'. $view. '" does not exist.');
+        extract($data);
+        ob_start();
+        include (self::parse($view));
+        $content = ob_get_clean();
+        if (!file_exists($layout)) echo $content;
+        else include (self::parse($layout));
+    }
     protected function tag_open($parser, $tag, $attr) {
-		$this->depth++;
-		if (count($this->content) || count($this->replace)) return ;
-		
-		foreach($attr as $key => $val) {
-			if (false !== ($i = strpos($key, self::PREFIX)) && $k = substr($key, $i+4)) {
-				if (in_array($k, array_keys($temp = array('condition' => 'if', 'repeat' => 'foreach')))) {
-					$this->{$k}[$this->depth] = $temp[$k];
-					echo "<?php {$temp[$k]}({$attr[$key]}):?>";
-				} elseif (in_array($k, array('content', 'replace'))) {
-					$this->{$k}[$this->depth] = $attr[$key];
-					if ('replace' === $k) {
-						unset($attr[$key]);
-						return ;
-					}
-				} else {
-					$attr[$k] = "<?php echo {$attr[$key]};?>";
-				}
-				unset($attr[$key]);
-			}
-		}
-		if (count($attr)) {
-			array_walk($attr, function(&$n, $i) { $n = $i. '="'. $n. '"';});
-			echo "<$tag ". implode(' ', $attr). ">";
-		} else echo "<$tag>";
+        $this->depth++;
+        if (!empty($this->content) || !empty($this->replace)) return ;
+        
+        foreach($attr as $key => $val) {
+            if (false !== ($i = strpos($key, self::PREFIX)) && $k = substr($key, $i+4)) {
+                if (in_array($k, array_keys($temp = array('condition' => 'if', 'repeat' => 'foreach')))) {
+                    $this->{$k}[$this->depth] = $temp[$k];
+                    echo "<?php {$temp[$k]}({$attr[$key]}):?>";
+                } elseif (in_array($k, array('content', 'replace'))) {
+                    $this->{$k}[$this->depth] = $attr[$key];
+                    if ('replace' === $k) {
+                        unset($attr[$key]);
+                        return ;
+                    }
+                } else {
+                    $attr[$k] = "<?php echo {$attr[$key]};?>";
+                }
+                unset($attr[$key]);
+            }
+        }
+        if (!empty($attr)) {
+            array_walk($attr, function(&$n, $i) { $n = $i. '="'. $n. '"';});
+            echo "<$tag ". implode(' ', $attr). ">";
+        } else echo "<$tag>";
     }
     protected function cdata($parser, $cdata) {
-		if (!count($this->content) && !count($this->replace)) echo $cdata;
+        if (empty($this->content) && empty($this->replace)) echo $cdata;
     }
     protected function tag_close($parser, $tag) {
-		foreach(array('content', 'replace') as $k) {
-			if (isset($this->{$k}[$this->depth])) {
-				if ($this->{$k}[$this->depth])
-					echo "<?php echo {$this->{$k}[$this->depth]};?>";
-				unset($this->{$k}[$this->depth]);
-				if ('replace' === $k) return $this->depth--;
-			}
-		}
-		if (!count($this->content) && !count($this->replace)) echo "</$tag>";
-		
-		foreach(array('condition' => 'if', 'repeat' => 'foreach') as $k => $v) {
-			if (isset($this->{$k}[$this->depth])) {
-				echo "<?php end$v;?>";
-				unset($this->{$k}[$this->depth]);
-			}
-		}
-		$this->depth--;
+        foreach(array('content', 'replace') as $k) {
+            if (isset($this->{$k}[$this->depth])) {
+                if ($this->{$k}[$this->depth])
+                    echo "<?php echo {$this->{$k}[$this->depth]};?>";
+                unset($this->{$k}[$this->depth]);
+                if ('replace' === $k) return $this->depth--;
+            }
+        }
+        if (empty($this->content) && empty($this->replace)) echo "</$tag>";
+        
+        foreach(array('condition' => 'if', 'repeat' => 'foreach') as $k => $v) {
+            if (isset($this->{$k}[$this->depth])) {
+                echo "<?php end$v;?>";
+                unset($this->{$k}[$this->depth]);
+            }
+        }
+        $this->depth--;
     }
 }
